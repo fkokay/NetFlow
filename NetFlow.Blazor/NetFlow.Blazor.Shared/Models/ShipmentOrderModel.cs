fd6e4aab482851c15076188dbeda207ad4ca1076
FATIH <fatihkokay@live.com>
2026-01-16 09:51:54 +0300

    edit

---
+++ NetFlow.Blazor/NetFlow.Blazor.Shared/Models/ShipmentOrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetFlow.Blazor.Shared.Models
+{
+    public class ShipmentOrderModel
+    {
+        public string OrderNo { get; }
+        public string StockCode { get; }
+        public decimal Quantity { get; }
+        public string Warehouse { get; }
+        public decimal AvailableStock { get; }
+    }
+}
---
+++ NetFlow.Blazor/NetFlow.Blazor.Shared/Models/ShippableOrdersFilterRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetFlow.Blazor.Shared.Models
+{
+    public class ShippableOrdersFilterRequest
+    {
+        public string Customer { get; set; } = string.Empty;
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? Warehouse { get; set; }
+        public bool HasBalance { get; set; }
+    }
+}
